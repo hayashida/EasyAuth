b8fb8313e20ff0fb7b95d454caed63fb8890a84c
hayashida <hayashida@k-useful.jp>
2012-10-16 14:22:32 +0900

    indent from tab to space

--- fuel/app/config/easyauth.php
+++ fuel/app/config/easyauth.php
@@ -2,40 +2,40 @@
 /**
  * EasyAuth
  *
- * @author		Yasuhiro Hayashida https://github.com/hayashida
- * @copyright	2012 Yasuhiro Hayashida
- * @license		MIT License
+ * @author       Yasuhiro Hayashida https://github.com/hayashida
+ * @copyright    2012 Yasuhiro Hayashida
+ * @license      MIT License
  */
 
 return array(
-	/**
-	 * DB connection, leave null to use default
-	 */
-	'db_connection' => null,
-	
-	/**
-	 * DB table name for the user table
-	 */
-	'table_name' => 'users',
-	
-	/**
-	 * DB field name for the user table
-	 */
-	'field_id' => 'id',
-	'field_username' => 'name',
-	'field_login_id' => 'login_id',
-	'field_password' => 'password',
-	'field_last_login' => 'last_login',
-	'field_login_hash' => 'login_hash',
-	
-	/**
-	 * Choose which columns are selected,
-	 *  must include: login_id, password, last_login, login_hash
-	 */
-	'table_columns' => array('*'),
-	
-	/**
-	 * Salt for the login hash
-	 */
-	'login_hash_salt' => '',
+    /**
+     * DB connection, leave null to use default
+     */
+    'db_connection' => null,
+    
+    /**
+     * DB table name for the user table
+     */
+    'table_name' => 'users',
+    
+    /**
+     * DB field name for the user table
+     */
+    'field_id'         => 'id',
+    'field_username'   => 'name',
+    'field_login_id'   => 'login_id',
+    'field_password'   => 'password',
+    'field_last_login' => 'last_login',
+    'field_login_hash' => 'login_hash',
+    
+    /**
+     * Choose which columns are selected,
+     *  must include: login_id, password, last_login, login_hash
+     */
+    'table_columns' => array('*'),
+    
+    /**
+     * Salt for the login hash
+     */
+    'login_hash_salt' => '',
 );
